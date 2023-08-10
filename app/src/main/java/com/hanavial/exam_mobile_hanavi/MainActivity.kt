a2c2e9ed2b013b3452017b79c7be7b9bce7ab64e
Hanavi Alvarel <hanavial@Hanavis-MacBook-Pro.local>
2023-08-10 17:18:33 +0700

    initial commit

---
+++ app/src/main/java/com/hanavial/exam_mobile_hanavi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hanavial.exam_mobile_hanavi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
